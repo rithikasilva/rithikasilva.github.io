43a3ba5e3282190137bc245c5cf0436a8553b4db
rithikasilva <53627672+rithikasilva@users.noreply.github.com>
2023-10-30 21:47:13 -0400

    Add ability to click on images -> not a modal yet

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -2,29 +2,35 @@ const imageList = [{ src: "photos/20231027_154808.jpg", alt: "image" }, { src: "
 
 const row = document.querySelector(".row");
 for (let i = 0; i < 3; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    row.appendChild(column);
+  const column = document.createElement("div");
+  column.classList.add("column");
+  row.appendChild(column);
 }
 
 // Distribute images into the columns
 for (let index = 0; index < imageList.length; index++) {
-    const columnIndex = index % 3;
-    const column = document.querySelectorAll(".column")[columnIndex];
+  const columnIndex = index % 3;
+  const column = document.querySelectorAll(".column")[columnIndex];
 
-    const img = document.createElement("img");
-    img.src = imageList[index].src;
-    img.alt = imageList[index].alt;
+  const a = document.createElement("a");
+  a.href = imageList[index].src; // Set the href to the image URL
 
-    img.addEventListener("click", () => {
-        lightbox.innerHTML = `<img src="${imageList[index].src}" alt="${imageList[index].alt}" loading="lazy">`;
-        lightbox.style.display = "block";
-    });
+  const img = document.createElement("img");
+  img.src = imageList[index].src;
+  img.alt = imageList[index].alt;
+  img.loading = "lazy";
 
-    column.appendChild(img);
+  // Add the click event to open the lightbox
+  img.addEventListener("click", () => {
+    lightbox.innerHTML = `<img src="${imageList[index].src}" alt="${imageList[index].alt}" loading="lazy">`;
+    lightbox.style.display = "block";
+  });
+
+  a.appendChild(img); // Wrap the image with the <a> element
+  column.appendChild(a); // Add the <a> element to the column
 }
 
 lightbox.addEventListener("click", () => {
-    lightbox.style.display = "none";
+  lightbox.style.display = "none";
 });
 
