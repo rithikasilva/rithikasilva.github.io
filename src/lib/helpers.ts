e52f2fbf05679b450ab4707cc89a5a1ce2c0d540
rithikasilva <rithikasilva@outlook.com>
2025-05-17 18:52:32 -0700

    feat: added functions for bay area timezone displaying

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -20,3 +20,28 @@ export function formatTimeForToronto(date: Date): string {
   formattedTime += " ET";
   return formattedTime;
 }
+
+export function getCurrentTimeInBayArea(): Date {
+  const now = new Date();
+
+  const offsetBayArea = -7;
+  now.setHours(now.getUTCHours() + offsetBayArea);
+
+  return now;
+}
+
+
+export function formatTimeForBayArea(date: Date): string {
+  const options: Intl.DateTimeFormatOptions = {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+    timeZone: "America/Los_Angeles",
+  };
+
+  let formattedTime = new Intl.DateTimeFormat("en-US", options).format(date);
+  formattedTime += " ET";
+  return formattedTime;
+}
+
